f32fe80737b24e8378052b4fe86661974582096b
lkush <l.kushnariova@yandex.ru>
2018-02-24 23:02:15 +0300

    Task implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let arr = equation.split(' ');
+    const a = +(arr[0]);
+    const b = +(arr[3] + arr[4]);
+    const c = +(arr[7] + arr[8]);
+
+    let discriminant = Math.sqrt(b ** 2 - 4 * a * c);
+    let x1 = (-b + discriminant) / (2 * a);
+    let x2 = (-b - discriminant) / (2 * a);
+    let result = [Math.round(x1), Math.round(x2)]
+    return result.sort((a, b) => a - b)
 }
